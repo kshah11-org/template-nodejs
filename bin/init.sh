80b98b3c68d03702987008a326c4516d43812997
Kush Shah <kush.shah@ibm.com>
2022-10-26 11:31:54 +0100

    feat: add git organisation as a variable

--- bin/init.sh
+++ bin/init.sh
@@ -4,14 +4,16 @@ gh auth login
 echo "Authorized GitHub"
 echo "Enter Project name:"
 read project
+read -p "Organisation [kshah11-org]: " organisation
+organisation=${username:-kshah11-org}
 template=$(git remote get-url --push origin)
 echo "Template is ${template}"
-gh repo create kshah11/${project} --template="${template}" --public
+gh repo create ${organisation}/${project} --template="${template}" --public
 echo "Succesfully initialized ${project}"
 cd ../
 echo "Navigated to parent folder"
 echo "Cloning Repo ${project}"
-gh repo clone kshah11/${project} ${project}
+gh repo clone ${organisation}/${project} ${project}
 echo "Repo Cloned"
 cd ${project}
 repo=$(git remote get-url --push origin)
@@ -20,8 +22,8 @@ jq '.name = "'${project}'"' package.json > "$tmp" && mv "$tmp" package.json
 jq '.description = "'${project}'"' package.json > "$tmp" && mv "$tmp" package.json
 echo "Package.json file updated name and description with value ${project}"
 jq '.repository.url = "git+'${repo}'"' package.json > "$tmp" && mv "$tmp" package.json
-jq '.bugs.url = "https://github.com/kshah11/'${project}'/issues"' package.json > "$tmp" && mv "$tmp" package.json
-jq '.homepage = "https://github.com/kshah11/'${project}'#readme"' package.json > "$tmp" && mv "$tmp" package.json
+jq '.bugs.url = "https://github.com/'${organisation}'/'${project}'/issues"' package.json > "$tmp" && mv "$tmp" package.json
+jq '.homepage = "https://github.com/'${organisation}'/'${project}'#readme"' package.json > "$tmp" && mv "$tmp" package.json
 echo "Package.json file updated relevant urls with value ${project}"
 sed -i '' "/sonar.projectKey=/ s/=.*/=kush-${project}/" sonar-project.properties
 sed -i '' "/sonar.projectName=/ s/=.*/=${project}/" sonar-project.properties
