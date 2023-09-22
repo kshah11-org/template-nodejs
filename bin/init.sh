4001ee739c3cc0626a70aba5c592a4258a65b21c
Kush Shah <kush.shah@ibm.com>
2023-09-22 12:56:01 +0100

    feat: change jumpstart for salesforce

--- bin/init.sh
+++ bin/init.sh
@@ -2,47 +2,40 @@
 echo "Authorizing GitHub"
 gh auth login
 echo "Authorized GitHub"
-echo "Enter Project name:"
+echo "Enter Project name (Spaces not allowed):"
 read project
 read -p "Organisation [kshah11-org]: " organisation
 organisation=${organisation:-kshah11-org}
-template=$(git remote get-url --push origin)
-echo "Template is ${template}"
-gh repo create ${organisation}/${project} --template="${template}" --public
-echo "Succesfully initialized ${project}"
 cd ../
-echo "Navigated to parent folder"
-echo "Cloning Repo ${project}"
-gh repo clone ${organisation}/${project} ${project}
-echo "Repo Cloned"
-FILE=.env
-if [ -f "$FILE" ]; then
-  echo "Copying snyk and sonar envs"
-  cp ./template-nodejs/${FILE} ./${project}/
-else
-  echo "Env file not found."
-fi
+sf project:generate --name ${project}
+cp -r ./template-nodejs/.github/ ${project}/.github
+cp -r ./template-nodejs/NOTES.md ${project}/NOTES.md
+cp -r ./template-nodejs/BACKLOG.md ${project}/BACKLOG.md
+cp -r ./template-nodejs/TECHDEBT.md ${project}/TECHDEBT.md
+cp -r ./template-nodejs/.prettierignore ${project}/.prettierignore
+cp -r ./template-nodejs/.nvmrc ${project}/.nvmrc
+cp -r ./template-nodejs/sonar-project.properties ${project}/sonar-project.properties
+cp -r ./template-nodejs/.env ${project}/.env
 cd ${project}
-repo=$(git remote get-url --push origin)
-tmp=$(mktemp)
-jq '.name = "'${project}'"' package.json > "$tmp" && mv "$tmp" package.json
-jq '.description = "'${project}'"' package.json > "$tmp" && mv "$tmp" package.json
-echo "Package.json file updated name and description with value ${project}"
-jq '.repository.url = "git+'${repo}'"' package.json > "$tmp" && mv "$tmp" package.json
-jq '.bugs.url = "https://github.com/'${organisation}'/'${project}'/issues"' package.json > "$tmp" && mv "$tmp" package.json
-jq '.homepage = "https://github.com/'${organisation}'/'${project}'#readme"' package.json > "$tmp" && mv "$tmp" package.json
-echo "Package.json file updated relevant urls with value ${project}"
+git init
+sf org create scratch -d -f config/project-scratch-def.json -a ${project}
+sf org display --json > org.json
+scratch_org_url=$(jq -r '.result.instanceUrl' org.json)
+scratch_org_token=$(jq -r '.result.accessToken' org.json)
+echo "SCRATCH_ORG_URL=${scratch_org_url}" >> .env
+echo "SCRATCH_ORG_TOKEN=${scratch_org_token}" >> .env
+sf config set org-instance-url=${scratch_org_url} --global
+sf project deploy start --source-dir force-app --ignore-conflicts --target-org ${scratch_org_token}
 sed -i '' "/sonar.projectKey=/ s/=.*/=kush-${project}/" sonar-project.properties
 sed -i '' "/sonar.projectName=/ s/=.*/=${project}/" sonar-project.properties
 echo "Sonar properties file updated project key and name with value ${project}"
-# export NVM_DIR=$HOME/.nvm
-# source $NVM_DIR/nvm.sh
+export NVM_DIR=$HOME/.nvm
+source $NVM_DIR/nvm.sh
 nvm use
 npm install
-npx snyk monitor
+npx snyk monitor --project-name=${project}
 git add .
-git commit -m "feat: update Package.json file"
-git push
-echo "Repo URL: ${repo}"
+git commit -m "feat: initial commit"
+gh repo create ${project} --push --public --source ./
 echo "Opening project in VSCode..."
 code .
